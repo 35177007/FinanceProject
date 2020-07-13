fe6045e03c42bee454ae8f2b793838c5e815a638
潇潇没有头发 <2919773237@qq.com>
2020-07-13 17:49:36 +0800

    新增一些bug

--- src/main/java/com/project/mapper/UserMapper.java
+++ src/main/java/com/project/mapper/UserMapper.java
@@ -8,4 +8,15 @@ import java.util.List;
 @Mapper
 public interface UserMapper {
     public List<User> selectUserAll();
+
+    public int addUser(User user);
+
+
+    public User selectUserById(Integer id);
+
+
+    public int updateUser(User user);
+
+
+    public int deleteUser(Integer id);
 }
